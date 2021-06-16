1abc242e1c35deba8cc7fba2d9ba3103bebebdc2
Anthony Cortese <akcortese@Anthonys-MacBook-Pro.local>
2021-06-16 06:04:49 -0600

    all tests passing except for 1, all sql queries pass

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,65 @@
-const router = require('express').Router()
+const {
+  checkAccountId,
+  checkAccountPayload,
+  checkAccountNameUnique,
+} = require("./accounts-middleware");
+const Account = require("./accounts-model");
 
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+const router = require("express").Router();
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+// /api/accounts
+router.get("/", (req, res, next) => {
+  Account.getAll()
+    .then((accounts) => {
+      res.status(200).json(accounts);
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+router.get("/:id", checkAccountId, (req, res, next) => {
+  res.status(200).json(req.account);
+});
 
-router.use((err, req, res, next) => { // eslint-disable-line
+router.post("/", checkAccountPayload, async (req, res, next) => {
+  try {
+    const created = await Account.create({
+      name: req.body.name.trim(),
+      budget: req.body.budget,
+    });
+    res.status(201).json(created);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put(
+  "/:id",
+  checkAccountPayload,
+  checkAccountId,
+  async (req, res, next) => {
+    try {
+      const updatedAccount = await Account.updateById(req.params.id, req.body);
+      res.status(200).json(updatedAccount);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.delete("/:id", checkAccountId, async (req, res, next) => {
+  try {
+    const deleteThis = await Account.deleteById(req.params.id);
+    res.status(200).json(deleteThis);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.use((err, req, res, next) => {
+  // eslint-disable-line
   // DO YOUR MAGIC
-})
+});
 
 module.exports = router;
